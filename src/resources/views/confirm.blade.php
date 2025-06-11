d88ee3e4b8e62b6966f8a257b174f9519d78563c
muto ryoko <obng3prn3@gmail.com>
2025-06-11 22:50:22 +0900

    bladeファイルの更新

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsection
+
+@section('title', 'confirm')
+
+@section('content')
+    
+@endsection
