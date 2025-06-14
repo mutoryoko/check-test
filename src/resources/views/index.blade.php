658878ca4973cc675d154d8d4e914abdfe41c0ed
muto ryoko <obng3prn3@gmail.com>
2025-06-15 02:52:22 +0900

    add session in index.blade

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -17,8 +17,8 @@
         <div class="contact-form__item contact-form__name">
           <p class="contact-form__label" for="last_name">お名前 <span class="required-mark">※</span></p>
           <div class="contact-form__input">
-            <input type="text" class="last_name__input" name="last_name" id="last_name" placeholder="例: 山田" value="{{ old('last_name') }}">
-            <input type="text" name="first_name" placeholder="例: 太郎" value="{{ old('first_name') }}">
+            <input type="text" class="last_name__input" name="last_name" id="last_name" placeholder="例: 山田" value="{{ old('last_name', session('form_input.last_name')) }}">
+            <input type="text" name="first_name" placeholder="例: 太郎" value="{{ old('first_name', session('form_input.first_name')) }}">
             @error('last_name')
               <p class="error">{{ $message }}</p>
             @enderror
@@ -44,7 +44,7 @@
         <div class="contact-form__item contact-form__email">
           <p class="contact-form__label">メールアドレス <span class="required-mark">※</span></p>
           <div class="contact-form__input">
-            <input type="email" name="email" id="email" placeholder="例: test@example.com" value="{{ old('email') }}">
+            <input type="email" name="email" id="email" placeholder="例: test@example.com" value="{{ old('email', session('form_input.email')) }}">
             @error('email')
               <p class="error">{{ $message }}</p>
             @enderror
@@ -54,11 +54,11 @@
         <div class="contact-form__item contact-form__tel">
           <p class="contact-form__label">電話番号 <span class="required-mark">※</span></p>
           <div class="contact-form__input">
-            <input type="tel" name="tel1" placeholder="080" value="{{ old('tel1') }}">
+            <input type="tel" name="tel1" placeholder="080" value="{{ old('tel1', session('form_input.tel1')) }}">
             <span class="hyphen">−</span>
-            <input type="tel" name="tel2" placeholder="1234" value="{{ old('tel2') }}">
+            <input type="tel" name="tel2" placeholder="1234" value="{{ old('tel2', session('form_input.tel2')) }}">
             <span class="hyphen">−</span>
-            <input type="tel" name="tel3" placeholder="5678" value="{{ old('tel3') }}">
+            <input type="tel" name="tel3" placeholder="5678" value="{{ old('tel3', session('form_input.tel3')) }}">
             @php
               $telError = collect([
                   $errors->first('tel1'), $errors->first('tel2'), $errors->first('tel3'),
@@ -73,7 +73,7 @@
         <div class="contact-form__item contact-form__address">
           <p class="contact-form__label">住所 <span class="required-mark">※</span></p>
           <div class="contact-form__input">
-            <input type="text" name="address" id="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address') }}">
+            <input type="text" name="address" id="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address', session('form_input.address')) }}">
             @error('address')
               <p class="error">{{ $message }}</p>
             @enderror
@@ -83,7 +83,7 @@
         <div class="contact-form__item contact-form__building">
           <p class="contact-form__label">建物名</p>
           <div class="contact-form__input">
-            <input type="text" name="building" id="building" placeholder="例: 千駄ヶ谷マンション101" value="{{ old('building') }}">
+            <input type="text" name="building" id="building" placeholder="例: 千駄ヶ谷マンション101" value="{{ old('building', session('form_input.building')) }}">
           </div>
         </div>
         {{-- カテゴリ --}}
@@ -107,7 +107,7 @@
         <div class="contact-form__item contact-form__detail">
           <p class="contact-form__label">お問い合わせの内容 <span class="required-mark">※</span></p>
           <div class="contact-form__textarea">
-            <textarea class="contact-form__textarea--text" name="detail" id="detail" cols="30" rows="5" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
+            <textarea class="contact-form__textarea--text" name="detail" id="detail" cols="30" rows="5" placeholder="お問い合わせ内容をご記載ください">{{ old('detail', session('form_input.detail')) }}</textarea>
             @error('detail')
               <p class="error contact-form__textarea--error">{{ $message }}</p>
             @enderror
